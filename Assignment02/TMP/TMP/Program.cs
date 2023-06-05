6e04897b15f4ec792942baea2902a44d9c999810
Dimitri Khanishvili <dkhanishvili@tbcbank.com.ge>
2023-06-05 11:04:49 +0400

    TMP

---
+++ Assignment02/TMP/TMP/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// დავალება 1
+Console.WriteLine("Enter TMP");
+bool validTmp = Double.TryParse(Console.ReadLine(), out double tmp);
+
+if (!validTmp)
+{
+    Console.Out.WriteLine("You enter invalid tmp number");
+    return;
+}
+
+if (tmp < 0)
+{
+    Console.Out.WriteLine("Freeze");
+}
+else if (tmp >= 0 && tmp <= 30)
+{
+    Console.WriteLine("Good Wather");
+} else
+{
+    Console.WriteLine("Hot");
+}
