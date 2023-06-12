e729f90fbc31aa66589d2db04911a22956128c9e
Dimitri Khanishvili <dkhanishvili@tbcbank.com.ge>
2023-06-12 12:25:21 +0400

    Assignment 05

---
+++ Assignment05/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] numbers = new int[] { 2, 127, 123, 0, 15 };
+int sum = sumOfDigit(numbers, 2);
+Console.WriteLine(sum);
+
+int sumOfDigit(int[] numbers, int index) {
+    int result = 0;
+    int number = 0;
+
+    for (int i = 0; i < numbers.Length; i++) {
+        if (i == index) {
+            number = numbers[i];
+            break;
+        }
+    }
+
+    while (number % 10 > 0) {
+        result += number % 10;
+        number = number / 10;
+    }
+
+    return result;
+}
---
+++ Assignment05/Task2/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter array size:");
+
+int arraySize;
+while (!int.TryParse(Console.ReadLine(), out arraySize) || arraySize <= 0)
+{
+    Console.WriteLine("Invalid number, try again:");
+}
+
+char[] charsArray = getArray(arraySize);
+
+Console.WriteLine("Enter char to find in array:");
+
+char ch;
+while (!char.TryParse(Console.ReadLine(), out ch))
+{
+    Console.WriteLine("Invalid char, try again:");
+}
+
+int numberOfChars = countCharInArray(charsArray, ch);
+printCharsInArray(numberOfChars, ch);
+
+char[] getArray(int arraySize)
+{
+    char[] result = new char[arraySize];
+
+    for (int i = 0; i < arraySize; i++) {
+        Console.WriteLine("Enter {0} element:", i + 1);
+
+        while (!char.TryParse(Console.ReadLine(), out result[i]))
+        {
+            Console.WriteLine("Invalid char, try again:");
+        }
+
+    }
+
+    return result;
+}
+
+int countCharInArray(char[] chars, char ch) {
+    int result = 0;
+
+    foreach(char c in chars)
+    {
+        if (c == ch) {
+            result++;
+        }
+    }
+
+    return result;
+}
+
+void printCharsInArray(int countOfChars, char ch)
+{
+    Console.WriteLine("Symbol '{0}' entered {1} times", ch, countOfChars);
+}
