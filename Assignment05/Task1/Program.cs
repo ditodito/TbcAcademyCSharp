c0f875c7825990c9f7272cb639b4d02e5b12a1ae
Dimitri Khanishvili <dkhanishvili@tbcbank.com.ge>
2023-06-18 11:38:42 +0400

    Fix Assignment05

--- Assignment05/Task1/Program.cs
+++ Assignment05/Task1/Program.cs
@@ -6,19 +6,31 @@ int sum = sumOfDigit(numbers, 2);
 Console.WriteLine(sum);
 
 int sumOfDigit(int[] numbers, int index) {
-    int result = 0;
     int number = 0;
 
-    for (int i = 0; i < numbers.Length; i++) {
+    /*for (int i = 0; i < numbers.Length; i++) {
         if (i == index) {
             number = numbers[i];
             break;
         }
-    }
+    }*/
 
-    while (number % 10 > 0) {
-        result += number % 10;
-        number = number / 10;
+    return getSumOfDigit(numbers[index]);
+}
+
+int getSumOfDigit(int n) {
+    int result = 0;
+    int number = Math.Abs(n);
+    int tempNumber;
+
+    while (number % 10 > 0)
+    {
+        tempNumber = number / 10;
+        
+        result = (tempNumber > 0) ? result + number % 10 :
+            (n > 0) ? result + number : result - number;
+    
+        number = tempNumber;
     }
 
     return result;
